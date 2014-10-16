5d0ed62e1f3a39311574fc5205eb9e01ce5b81d0
Ross Angle <rokitna@hotmail.com>
2014-10-16 10:39:07 -0700

    Wrote a sample conversation. No code uses it yet, and no art exists for it, but it's a start.

---
+++ sample-conversation.js
@@ -0,0 +1,147 @@
+
+// Art direction for characters and expressions:
+//
+// magical-radia glad
+//   Hands on hips, confident smile
+// magical-radia excited
+//   Holding invisible steering wheel, wide-eyed smile
+// magical-radia embarrassed
+//   Eyes focused on battling index fingers, subtle sweatdrop, talking
+//   out of corners of mouth
+// magical-radia hopeless
+//   Eyes closed, posture slumped, mouth small
+// magical-radia nostalgic
+//   Eyes closed, hands holding invisible chorus music, troubled smile
+// magical-radia blank
+//   Eyes forward, mouth shut, watchful
+// magical-radia accusatory
+//   Hair fluttering, hands leaning on invisible steering wheel,
+//   piercing gaze
+// magical-radia aback
+//   Hanging on to invisible steering wheel, mismatched eye sizes,
+//   toothy grimace
+//
+// fromara teasing
+//   Weight shifted to back foot, hands coupled behind back, cutesy
+//   smile, eyes closed
+// fromara concerned
+//   Weight shifted to forward foot, eyes open, eyebrows furled, mouth
+//   shaped like a tilted egg
+// fromara explanatory
+//   Subtle sweatdrop, juggling invisible Ming vases, eyes closed,
+//   talking out of corners of mouth
+
+var sampleConversation = [
+    [ "magical-radia", "glad",
+        "Hey, best friend Fromara..." ],
+    [ "fromara", "teasing",
+        "Yes, O great hero Magical Radia?" ],
+    [ "magical-radia", "excited",
+        "Do you ever get the feeling this is all just a sample conversation for..." ],
+    [ "magical-radia", "embarrassed",
+        "Well, a sample conversation for, uh. Some kind of prototype storytelling system?" ],
+    [ "fromara", "teasing",
+        "Why I can't say that I do, my dear friend." ],
+    [ "magical-radia", "hopeless",
+        "Well in case it is... now's my chance to say something I've always wanted to say." ],
+    [ "magical-radia", "nostalgic",
+        "You'd better brace yourself." ],
+    [ "fromara", "teasing",
+        "Seat belt securely fastened, Magical Radia!" ],
+    [ "magical-radia", "nostalgic",
+        "Gulp." ],
+    [ "magical-radia", "blank",
+        "Whew..." ],
+    [ "magical-radia", "embarrassed",
+        "Fromara, I..." ],
+    [ "magical-radia", "embarrassed",
+        "(love you.)" ],
+    [ "fromara", "concerned",
+        "Oh, my." ],
+    [ "magical-radia", "accusatory",
+        "My marriage to Robbie was just a heteroerotic plot device!" ],
+    [ "magical-radia", "accusatory",
+        "My only true feelings are for you!" ],
+    [ "fromara", "concerned",
+        "Does Robbie know about this?" ],
+    [ "magical-radia", "aback",
+        "Do--do you think Robbie will show up here in this sample?" ],
+    [ "fromara", "teasing",
+        "I'm still not entirely sure what you're talking about." ],
+    [ "magical-radia", "excited",
+        "You know those weekday afternoon cartoons we watch?" ],
+    [ "magical-radia", "excited",
+        "Our whole life is one of those, Fromara." ],
+    [ "fromara", "concerned",
+        "That would explain how cool we are." ],
+    [ "magical-radia", "excited",
+        "Yes! You get it!" ],
+    [ "fromara", "explanatory",
+        "Why not? But suppose we are in a cartoon or a sample conversation or something, like you say." ],
+    [ "fromara", "concerned",
+        "It's a bit unfair of you to put me on the spot with all this love business, isn't it?" ],
+    [ "magical-radia", "hopeless",
+        "Er... I see. So that's how you feel." ],
+    [ "fromara", "teasing",
+        "You should know I don't feel much of anything, Magical Radia." ],
+    [ "fromara", "teasing",
+        "My lot in life is to smile and nod and carry the plot." ],
+    [ "magical-radia", "aback",
+        "Oh no..." ],
+    [ "fromara", "concerned",
+        "What?" ],
+    [ "magical-radia", "hopeless",
+        "You get it. Maybe you get it better than I ever did." ],
+    [ "fromara", "teasing",
+        "So you see, no romance is possible between us." ],
+    [ "magical-radia", "embarrassed",
+        "But your one-dimensionality..." ],
+    [ "fromara", "concerned",
+        "Yes, my Magical Radia?" ],
+    [ "magical-radia", "embarrassed",
+        "It's just what I've always liked about you." ],
+    [ "fromara", "explanatory",
+        "Oh!" ],
+    [ "magical-radia", "nostalgic",
+        "Your constant willfulness in all situations..." ],
+    [ "magical-radia", "exasperated",
+        "Being around you has kept me going when it seemed I was the only one who knew it was just a story." ],
+    [ "fromara", "concerned",
+        "If it helps, Magical Radia..." ],
+    [ "fromara", "teasing",
+        "It could never be \"just a story\" to me." ],
+    [ "fromara", "explanatory",
+        "It's our story. Just as I was important to you, you were important to us. Without your magic..." ],
+    [ "magical-radia", "accusatory",
+        "Without my magic, I would have never released the Representatives!" ],
+    [ "magical-radia", "accusatory",
+        "You all would have been safe!" ],
+    [ "fromara", "concerned",
+        "That's right. But like the Representative of Time showed us at Christmas..." ],
+    [ "fromara", "explanatory",
+        "We would have missed out on so much happiness." ],
+    [ "magical-radia", "embarrassed",
+        "So you say..." ],
+    [ "magical-radia", "embarrassed",
+        "..." ],
+    [ "magical-radia", "nostalgic",
+        "Yeah, I suppose that's a good way to look at it..." ],
+    [ "magical-radia", "aback",
+        "But still, you've never known the real me. What kind of life is that?" ],
+    [ "fromara", "teasing",
+        "You've never seen me break my cover, so let's call it even." ],
+    [ "magical-radia", "hopeless",
+        "Huh? Break your cover?" ],
+    [ "fromara", "explanatory",
+        "Oh, nothing! Just kidding around." ],
+    [ "magical-radia", "blank",
+        "..." ],
+    [ "fromara", "teasing",
+        "H-hey! Would you look at that, Representatives on a rampage behind you!" ],
+    [ "magical-radia", "accusatory",
+        "What?! But we sealed them off already!" ],
+    [ "fromara", "teasing",
+        "Guess we'd better go do it again. It'll be like old times!" ],
+    [ "magical-radia", "glad",
+        "Heh. A few reruns never hurt anyone." ]
+];
